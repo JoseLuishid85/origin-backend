499419308f09a07e312d889e327718891d5f32a2
unknown <joseluishid01@gmail.com>
2026-06-24 21:23:34 -0400

    Revision

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,4 +1,4 @@
-const { Product, Department, ProductType, ProductUse } = require('../models/associations');
+const { Product, Department, ProductType, ProductUse, Inventory, Branch, Deposit } = require('../models/associations');
 
 const createProduct = async (req, res) => {
     const data = req.body;
@@ -108,10 +108,69 @@ const deleteProduct = async (req, res) => {
     }
 };
 
+const getProductInventoryByBranch = async (req, res) => {
+    const { branchId } = req.params;
+
+    try {
+        const branchExists = await Branch.findByPk(branchId);
+        if (!branchExists) {
+            return res.status(404).json({ msg: `No existe una sucursal con el id ${branchId}` });
+        }
+
+        const inventories = await Inventory.findAll({
+            where: { branchId, state: true },
+            include: [
+                { model: Product, as: 'product', attributes: ['id', 'name', 'packageType', 'packageQuantity'] },
+                { model: Deposit, as: 'deposit', attributes: ['id', 'name', 'main'] }
+            ]
+        });
+
+        const productMap = {};
+        inventories.forEach(inv => {
+            const pid = inv.productId;
+            if (!productMap[pid]) {
+                productMap[pid] = {
+                    productId: pid,
+                    product: inv.product,
+                    totalStock: 0,
+                    stockMin: inv.stockMin,
+                    stockOrder: inv.stockOrder,
+                    deposits: []
+                };
+            }
+            productMap[pid].totalStock += inv.stock;
+            if (inv.deposit.main) {
+                productMap[pid].stockMin = inv.stockMin;
+                productMap[pid].stockOrder = inv.stockOrder;
+            }
+            productMap[pid].deposits.push({
+                depositId: inv.deposit.id,
+                depositName: inv.deposit.name,
+                stock: inv.stock
+            });
+        });
+
+        const products = Object.values(productMap).map(p => ({
+            ...p,
+            belowMin: p.totalStock <= p.stockMin,
+            belowOrder: p.totalStock <= p.stockOrder
+        }));
+
+        res.json({
+            branch: { id: branchExists.id, name: branchExists.name },
+            products
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ msg: "Error al obtener el inventario de productos" });
+    }
+};
+
 module.exports = {
     createProduct,
     getProducts,
     getProductById,
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    getProductInventoryByBranch
 };
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -5,13 +5,15 @@ const {
     getProducts,
     getProductById,
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    getProductInventoryByBranch
 } = require('../controllers/productControllers.js');
 
 const routes = express.Router();
 
 routes.post('/', checkAuth, createProduct);
 routes.get('/', checkAuth, getProducts);
+routes.get('/inventory/branch/:branchId', checkAuth, getProductInventoryByBranch);
 routes.get('/:id', checkAuth, getProductById);
 routes.put('/:id', checkAuth, updateProduct);
 routes.delete('/:id', checkAuth, deleteProduct); // Endpoint Delete agregado
